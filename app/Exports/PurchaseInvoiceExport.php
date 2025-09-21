f904a97bab1b146a0226e53a1ccdf347a25f28bc
ahmed0801 <ahmedarfaoui1600@gmailcom>
2025-09-21 18:54:47 +0200

    second push 55555

--- app/Exports/PurchaseInvoiceExport.php
+++ app/Exports/PurchaseInvoiceExport.php
@@ -17,32 +17,49 @@ class PurchaseInvoiceExport implements FromCollection, WithHeadings
         $this->invoice = $invoice;
     }
 
-    public function collection(): Collection
-    {
-        $data = collect();
+   public function collection(): Collection
+{
+    $data = collect();
 
-        // Ajouter les informations de la facture
-        $data->push([
-            'Type' => 'Facture',
-            'Numéro' => $this->invoice->numdoc,
-            'Fournisseur' => $this->invoice->supplier->name ?? '',
-            'Date Facture' => \Carbon\Carbon::parse($this->invoice->invoice_date)->format('d/m/Y'),
-            'Statut' => ucfirst($this->invoice->status),
-            'Type Facture' => ucfirst($this->invoice->type),
-            'Total HT' => number_format($this->invoice->total_ht, 2) . ' €',
-            'Total TTC' => number_format($this->invoice->total_ttc, 2) . ' €',
-            'Code Article' => '',
-            'Désignation' => '',
-            'Quantité' => '',
-            'PU HT' => '',
-            'Remise (%)' => '',
-            'TVA (%)' => '',
-            'Total Ligne' => '',
-        ]);
+    $data->push((object)[
+        'Type' => 'Facture',
+        'Numéro' => $this->invoice->numdoc,
+        'Fournisseur' => $this->invoice->supplier->name?? '',
+        'Date Facture' => \Carbon\Carbon::parse($this->invoice->invoice_date)->format('d/m/Y'),
+        'Statut' => ucfirst($this->invoice->status),
+        'Type Facture' => ucfirst($this->invoice->type),
+        'Total HT' => number_format($this->invoice->total_ht, 2). ' €',
+        'Total TTC' => number_format($this->invoice->total_ttc, 2). ' €',
+        'Code Article' => '',
+        'Désignation' => '',
+        'Quantité' => '',
+        'PU HT' => '',
+        'Remise (%)' => '',
+        'TVA (%)' => '',
+        'Total Ligne' => '',
+    ]);
 
-        // Ajouter une ligne vide pour séparation
-        $data->push([
-            'Type' => '',
+    $data->push((object)[
+        'Type' => '',
+        'Numéro' => '',
+        'Fournisseur' => '',
+        'Date Facture' => '',
+        'Statut' => '',
+        'Type Facture' => '',
+        'Total HT' => '',
+        'Total TTC' => '',
+        'Code Article' => '',
+        'Désignation' => '',
+        'Quantité' => '',
+        'PU HT' => '',
+        'Remise (%)' => '',
+        'TVA (%)' => '',
+        'Total Ligne' => '',
+    ]);
+
+    foreach ($this->invoice->lines as $line) {
+        $data->push((object)[
+            'Type' => 'Ligne',
             'Numéro' => '',
             'Fournisseur' => '',
             'Date Facture' => '',
@@ -50,38 +67,18 @@ class PurchaseInvoiceExport implements FromCollection, WithHeadings
             'Type Facture' => '',
             'Total HT' => '',
             'Total TTC' => '',
-            'Code Article' => '',
-            'Désignation' => '',
-            'Quantité' => '',
-            'PU HT' => '',
-            'Remise (%)' => '',
-            'TVA (%)' => '',
-            'Total Ligne' => '',
+            'Code Article' => $line->article_code?? '-',
+            'Désignation' => $line->item->name?? $line->description?? '-',
+            'Quantité' => $line->quantity,
+            'PU HT' => number_format($line->unit_price_ht, 2). ' €',
+            'Remise (%)' => $line->remise. '%',
+            'TVA (%)' => number_format($line->tva, 2). '%',
+            'Total Ligne' => number_format($line->total_ligne_ht, 2). ' €',
         ]);
+}
 
-        // Ajouter les lignes de la facture
-        foreach ($this->invoice->lines as $line) {
-            $data->push([
-                'Type' => 'Ligne',
-                'Numéro' => '',
-                'Fournisseur' => '',
-                'Date Facture' => '',
-                'Statut' => '',
-                'Type Facture' => '',
-                'Total HT' => '',
-                'Total TTC' => '',
-                'Code Article' => $line->article_code ?? '-',
-                'Désignation' => $line->item->name ?? $line->description ?? '-',
-                'Quantité' => $line->quantity,
-                'PU HT' => number_format($line->unit_price_ht, 2) . ' €',
-                'Remise (%)' => $line->remise . '%',
-                'TVA (%)' => number_format($line->tva, 2) . '%',
-                'Total Ligne' => number_format($line->total_ligne_ht, 2) . ' €',
-            ]);
-        }
-
-        return $data;
-    }
+    return $data;
+}
 
     public function headings(): array
     {
