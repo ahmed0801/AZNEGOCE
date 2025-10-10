4b211315a1f6cebd99e3efc94a5670a30c55e4d7
rayan <rayan.ouerdane@epitech.eu>
2025-10-10 15:46:11 +0200

    feat(app): update bot and view

--- resources/views/voice.blade.php
+++ resources/views/voice.blade.php
@@ -691,8 +691,8 @@ body {
   border: none;
   background: transparent;
   cursor: pointer;
-  width: 35px;
-  height: 35px;
+  width: 2.5rem;
+  height: 2.5rem;
   padding: 0;
   display: flex;
   justify-content: center;
