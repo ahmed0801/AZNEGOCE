d97d9bee7eed682eb386c3c11d4e786f424df484
ahmed0801 <ahmedarfaoui1600@gmailcom>
2025-10-11 20:42:54 +0200

    second push

---
+++ app/Console/Kernel copy.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+
+
+
+    protected function schedule(\Illuminate\Console\Scheduling\Schedule $schedule)
+{
+    // Exécution chaque nuit à 02h00
+    $schedule->command('golda:import')->dailyAt('18:38');
+}
+
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,13 +7,15 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
- protected function schedule(Schedule $schedule)
-    {
-        // Exécution toutes les minutes pour le test
-        $schedule->command('golda:import')
-            ->everyMinute()
-            ->appendOutputTo(storage_path('logs/golda_schedule.log'));
-    }
+
+
+        protected function schedule(\Illuminate\Console\Scheduling\Schedule $schedule)
+{
+    // Exécution chaque nuit à 02h00
+    $schedule->command('golda:import')->dailyAt('20:45');
+}
+
+
 
     protected function commands()
     {
