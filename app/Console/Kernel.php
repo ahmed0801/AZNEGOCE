4d9dd21cb37c9d12a15d586658a99360b0b9d6b4
ahmed0801 <ahmedarfaoui1600@gmailcom>
2025-10-14 23:41:55 +0200

    second push

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
         protected function schedule(\Illuminate\Console\Scheduling\Schedule $schedule)
 {
     // Exécution chaque nuit à 02h00
-    $schedule->command('golda:import')->dailyAt('23:45')
+    $schedule->command('golda:import')->dailyAt('23:50')
     ->appendOutputTo(storage_path('logs/golda_schedule.log'))
         ->timezone('Europe/Paris');
 }
